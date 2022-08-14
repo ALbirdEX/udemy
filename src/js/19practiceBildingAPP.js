45ed3c80451b2fdddfc0b5993daa18899c49bf21
ALbirdEX <alex123890@gmail.com>
2022-08-14 15:36:22 +0300

    Add 19practiceBildingAPP.

---
+++ src/js/19practiceBildingAPP.js
@@ -0,0 +1,23 @@
+// lessons 19
+// "use strict"
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); //ставим унарный + т.к. работаем с числами
+console.log(numberOfFilms)
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false,
+};
+
+const a = prompt('Один из просмотренных фильмов?', ''),
+    b = +prompt('На сколько оцениваете его?', ''),
+    c = prompt('Один из просмотренных фильмов?', ''),
+    d = +prompt('На сколько оцениваете его?', '');
+
+personalMovieDB.movies[a] = b;  // записываем свойства в объект,
+personalMovieDB.movies[c] = d;  // записываем в ключ - lastMovie, в значение -  reiting.
+
+console.log(personalMovieDB)
